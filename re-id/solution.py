6ff117e829c5ebc09ee0cc7650a894d6da929266
Rishu Anand <riuandg5@gmail.com>
2022-09-12 02:47:20 +0530

    solve re-id

--- re-id/solution.py
+++ re-id/solution.py
@@ -1,2 +1,16 @@
 def solution(i):
     # Your code here
+    primes_string = "2"
+    primes_arr = [2]
+    num = 3
+    while (len(primes_string) <= i+5):
+        idx = 0
+        while (primes_arr[idx]*primes_arr[idx] <= num):
+            if (num%primes_arr[idx] == 0):
+                num += 2
+                idx = 0
+            idx += 1
+        primes_arr.append(num)
+        primes_string += str(num)
+        num += 2
+    return primes_string[i:i+5]
---
+++ re-id/test.py
@@ -0,0 +1,5 @@
+from solution import solution
+
+print(solution(0))
+print(solution(3))
+print(solution(10000))
