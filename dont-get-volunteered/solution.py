c61fc80df383df691a2469ecf08ea79114b2a0ba
Rishu Anand <riuandg5@gmail.com>
2022-09-18 18:43:06 +0530

    solve dont-get-volunteered

--- dont-get-volunteered/solution.py
+++ dont-get-volunteered/solution.py
@@ -1,2 +1,35 @@
 def solution(src, dest):
     #Your code here
+    size = 8
+    board = [[None for i in range(size)] for j in range(size)]
+
+    board[src//size][src%size] = 0
+    fill = 0
+
+    while (board[dest//size][dest%size] == None):
+        for i in range(size):
+            for j in range(size):
+                if (board[i][j] == fill):
+                    if (i+2<size and j+1<size and board[i+2][j+1]==None):
+                        board[i+2][j+1] = fill+1
+                    if (i+2<size and j-1>=0 and board[i+2][j-1]==None):
+                        board[i+2][j-1] = fill+1
+
+                    if (i-2>=0 and j+1<size and board[i-2][j+1]==None):
+                        board[i-2][j+1] = fill+1
+                    if (i-2>=0 and j-1>=0 and board[i-2][j-1]==None):
+                        board[i-2][j-1] = fill+1
+
+                    if (i+1<size and j+2<size and board[i+1][j+2]==None):
+                        board[i+1][j+2] = fill+1
+                    if (i-1>=0 and j+2<size and board[i-1][j+2]==None):
+                        board[i-1][j+2] = fill+1
+
+                    if (i+1<size and j-2>=0 and board[i+1][j-2]==None):
+                        board[i+1][j-2] = fill+1
+                    if (i-1>=0 and j-2>=0 and board[i-1][j-2]==None):
+                        board[i-1][j-2] = fill+1
+                        
+        fill = fill + 1
+
+    return board[dest//size][dest%size]
---
+++ dont-get-volunteered/test.py
@@ -0,0 +1,4 @@
+from solution import solution
+
+print(solution(0, 1))
+print(solution(19, 36))
